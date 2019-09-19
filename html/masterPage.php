bc242be109bb075546e42f8ee734f4e260600780
Foxmaner <eskil.brann@gmail.com>
2019-09-19 22:19:22 +0200

    added wrapper for greenieBoard

--- html/masterPage.php
+++ html/masterPage.php
@@ -1,3 +1,38 @@
-<section id="greenieboard" class="col-s-12 col-10">
-  master!
+<section id="greenieboard" class="col-s-12 col-8">
+  <table id="greenieTable">
+    <tr class="greenieRow">
+      <td class="tdCallsign">Cool</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </table>
 </section>
