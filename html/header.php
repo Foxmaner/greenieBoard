c9a8f6e0fe8ebf7712ec1a624ea599fae6e5191a
Foxmaner <eskil.brann@gmail.com>
2019-09-19 13:15:57 +0200

    added icon

--- html/header.php
+++ html/header.php
@@ -1,5 +1,20 @@
 <header id="indexHeader">
+<?php
 
+
+  if (isset($_SESSION["isLoggedIn"])) {
+    if ($_SESSION["isLoggedIn"]==true) {
+      
+    }else{
+      echo "<button id='toggleLoginBtn'>Login</button>";
+    }
+  }else{
+    echo "<button id='toggleLoginBtn'>Login</button>";
+  }
+  ?>
+
+
+<img id="headerIcon" src="./media/pictures/MA-Icon.png"> </img>
 
 
 
