60632d141dd4c480e40166ca327f4440be39e918
Foxmaner <eskil.brann@gmail.com>
2019-09-27 20:47:45 +0200

    Added basic table

--- html/masterPage.php
+++ html/masterPage.php
@@ -1,8 +1,40 @@
 <section id="greenieboard" class="col-s-12 col-8">
   <table id="greenieTable">
     <tr class="greenieRow">
-      <td class="tdCallsign">Cool</td>
+      <td class="tdCallsign">Callsign</td>
       <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr class="greenieRow">
+      <td class="tdCallsign">Callsign</td>
       <td></td>
       <td></td>
       <td></td>
